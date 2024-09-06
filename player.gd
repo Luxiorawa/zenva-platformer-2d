9df33556298ba019f2429365d6271030e3b746ca
Clément Noël <noyelclement@hotmail.fr>
2024-09-06 12:03:23 +0200

    feat: add all sprites, implement tilemap (4.3) and add player movements

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var move_speed: float = 100.0
+var jump_force: float = 200.0
+var gravity: float = 500.0
+
+# delta est une valeur permettant, quand utilisé, de convertir une action "per frame" en "per second". (Rappel que la fonction _physics_process est appelée à chaque frame)
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	velocity.x = 0;
+
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= move_speed
+
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += move_speed
+
+	if Input.is_key_pressed(KEY_UP) and is_on_floor():
+		velocity.y = -jump_force
+	
+
+	move_and_slide()
